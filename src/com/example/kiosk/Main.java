c40fa9fdfa2721bdcff0356bcce612b99e5d7c04
escomputer <nujest307@naver.com>
2025-03-11 12:40:42 +0900

    Feat : 종료기능
    0 입력시 , 종료
    선택한 메뉴 항목 출력

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -15,12 +15,35 @@ public class Main {
             menuList.add(new MenuItem("Cheesburger",6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
             menuList.add(new MenuItem("Hamburger",5.4, "고기 패티만 들어간 기본 버거"));
 
+        Scanner sc = new Scanner(System.in);
 
-            for ( int i=0; i<menuList.size();i++){
-                System.out.println((i+1)+". "+menuList.get(i));
+
+        while(true) {
+
+            for (int i = 0; i < menuList.size(); i++) {
+                System.out.println((i + 1) + ". " + menuList.get(i));
 
             }
             System.out.println("0. 종료");
+
+            System.out.println(" 번호를 선택해주세요. ");
+            int idx=sc.nextInt();
+
+            if (idx==0) {
+                System.out.println("키오스크를 종료합니다.");
+                break;
+            }
+            else {
+                System.out.println("=============추가============");
+                System.out.println(idx+". "+menuList.get(idx-1));
+                System.out.println("============================");
+            }
+
+        }
+
+
+
+
 
 //
 
