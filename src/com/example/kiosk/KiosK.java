640bb5e3fe55350a4f41671804d50d6651f0c271
escomputer <nujest307@naver.com>
2025-03-11 21:53:33 +0900

    Feat : 키오스크 카테고리 출력
    Menu클래스에 접근하여서 리스트를 사용한다.
    새로운 카테고리를 추가할 수 있다.
    start 메서드에서는 번호 입력받으면 그에
    해당하는 카테고리가 반환되게끔한다.

--- src/com/example/kiosk/KiosK.java
+++ src/com/example/kiosk/KiosK.java
@@ -5,20 +5,29 @@ import java.util.List;
 import java.util.Scanner;
 
 public class KiosK {
-    List<MenuItem> menuItems = new ArrayList<>();
 
-    public KiosK(List<MenuItem> menuItems){
-        this.menuItems=menuItems;
+    //속성
+    private List<Menu> menus ; // Menu에서 관리되는 리스트를 활용한다.
+
+
+    //생성자
+    public KiosK(List<Menu> menus){
+        this.menus = menus;
     }
 
+
+    //기능
+    public void addMenu(Menu menu){
+        menus.add(menu);
+    }
     public void start(){
         Scanner sc = new Scanner(System.in);
 
 
         while(true) {
-
-            for (int i = 0; i < menuItems.size(); i++) {
-            System.out.println((i + 1) + ". " + menuItems.get(i));
+            System.out.println("어떤 메뉴를 드시겠습니까?");
+            for (int i = 0; i < menus.size(); i++) {
+            System.out.println((i + 1) + ". " + menus.get(i).getCategory());
 
             }
             System.out.println("0. 종료");
@@ -30,12 +39,13 @@ public class KiosK {
                 System.out.println("키오스크를 종료합니다.");
                 break;
             }
-            else if (idx>0 && idx<=menuItems.size()){
+            else if (idx>0 && idx<=menus.size()){
                 System.out.println("=============추가============");
-                System.out.println(idx+". "+menuItems.get(idx-1));
+                System.out.println(idx+". "+menus.get(idx-1));
                 System.out.println("============================");
             } else System.out.println("항목에 있는 번호를 입력해주세요.");
     }
 
-
-}}
+        sc.close();
+}
+}
