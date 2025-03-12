c2194d970b1cb6e8618de25d6fc55515a479ae94
escomputer <nujest307@naver.com>
2025-03-12 12:35:02 +0900

    sc.close 삽입
    각 메서드마다 scanner를 다 닫아 버리니깐
    뒤로가기 하고나서 오류가 생김.
    그래서 반복이 아예 끝나는 start의 말단에
    close()를 넣음.

--- src/com/example/kiosk/KiosK.java
+++ src/com/example/kiosk/KiosK.java
@@ -7,94 +7,95 @@ import java.util.Scanner;
 public class KiosK {
 
     //속성
-    private List<Menu> menus ; // Menu에서 관리되는 리스트를 활용한다.
+    private List<Menu> menus; // Menu에서 관리되는 리스트를 활용한다.
 
 
     //생성자
-    public KiosK(List<Menu> menus){
+    public KiosK(List<Menu> menus) {
         this.menus = menus;
     }
 
 
     //기능
-    public void addMenu(Menu menu){
+    public void addMenu(Menu menu) {
         menus.add(menu);
     }
-    public void start(){
+
+    public void start() {
         Scanner sc = new Scanner(System.in);
 
 
-        while(true) {
+        while (true) {
             System.out.println("어떤 메뉴를 드시겠습니까?");
             for (int i = 0; i < menus.size(); i++) {
-            System.out.println((i + 1) + ". " + menus.get(i).getCategory());
+                System.out.println((i + 1) + ". " + menus.get(i).getCategory());
 
             }
             System.out.println("0. 종료");
 
-            try{
+            try {
                 System.out.println("번호를 선택해주세요. ");
-                int idx=sc.nextInt();
-                if(idx<0 || idx>menus.size()){
+                int idx = sc.nextInt();
+                if (idx < 0 || idx > menus.size()) {
                     throw new IllegalArgumentException("항목에 있는 번호를 눌러주세요.");
                 }
-                if (idx==0) {
+                if (idx == 0) {
                     System.out.println("키오스크를 종료합니다.");
 
                     break;
                 }
 
-                showMenus(menus.get(idx-1)); // 두 조건문에 안걸림 -> 정상 입력 됐을때 처리하는
+                showMenus(menus.get(idx - 1)); // 두 조건문에 안걸림 -> 정상 입력 됐을때 처리하는
 
 
-            }catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("숫자를 입력해주세요.");
                 sc.nextLine(); //버퍼 비우기 !!
-            }catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
 
+        }
+
+        sc.close(); // start -> showMenus -> start 할 경우 대비해서 start가 완전히 끝날때 sc.close()
     }
 
-
-}
-private void showMenus(Menu menu){
+    private void showMenus(Menu menu) {
         Scanner sc = new Scanner(System.in);
 
-    while(true) {
-        List<MenuItem> items = menu.getMenu();
-        System.out.println("\n===="+menu.getCategory()+"====");
-        for (int i = 0; i < items.size(); i++) {
-            System.out.println((i + 1) + ". " + items.get(i));
+        while (true) {
+            List<MenuItem> items = menu.getMenu();
+            System.out.println("\n====" + menu.getCategory() + "====");
+            for (int i = 0; i < items.size(); i++) {
+                System.out.println((i + 1) + ". " + items.get(i));
 
-        }
-        System.out.println("0. 뒤로가기");
+            }
+            System.out.println("0. 뒤로가기");
 
-        try{
-            System.out.println("번호를 선택해주세요. ");
+            try {
+                System.out.println("번호를 선택해주세요. ");
 
-        int idx=sc.nextInt();
+                int idx = sc.nextInt();
 
-        if (idx==0) {
-            break; // 반복문 탈출 -> start()처음으로 돌아감.
-        }
-        if (idx<0 || idx>items.size()){
-            throw new IllegalArgumentException("항목에 있는 번호를 눌러주세요.");
-        }
-            System.out.println("=============추가============");
-            System.out.println(idx+". "+items.get(idx-1));
-            System.out.println("============================");
-        }catch (InputMismatchException e){
-            System.out.println("숫자를 입력해주세요.");
-            sc.nextLine(); //버퍼 비우기 !!
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            sc.nextLine();
+                if (idx == 0) {
+                    break; // 반복문 탈출 -> start()처음으로 돌아감.
+                }
+                if (idx < 0 || idx > items.size()) {
+                    throw new IllegalArgumentException("항목에 있는 번호를 눌러주세요.");
+                }
+                System.out.println("=============추가============");
+                System.out.println(idx + ". " + items.get(idx - 1));
+                System.out.println("============================");
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력해주세요.");
+                sc.nextLine(); //버퍼 비우기 !!
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
     }
-
-}
 
 
 }
