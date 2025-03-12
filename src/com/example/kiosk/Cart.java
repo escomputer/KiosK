4c21a881fb23e5177d78cc6b8cb86e5cb7eefe08
escomputer <nujest307@naver.com>
2025-03-12 14:25:19 +0900

    장바구니 addCart 메서드 추가
    MenuItem의 getter를 통해 이름을 출력하는 기능 +
    리스트에 item 추가하는 기능
    이 담긴 메서드 추가

--- src/com/example/kiosk/Cart.java
+++ src/com/example/kiosk/Cart.java
@@ -1,4 +1,18 @@
 package com.example.kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+
+    private List<MenuItem> selectToCart;
+
+    public Cart(){
+        this.selectToCart = new ArrayList<>();
+    }
+
+    public void addCart(MenuItem item){
+        selectToCart.add(item);
+        System.out.println(item.getName()+"이 장바구니에 추가되었습니다.");
+    }
 }
