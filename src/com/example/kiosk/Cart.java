dc5730a1bff3bc72bcc93187a17753a1e3250596
escomputer <nujest307@naver.com>
2025-03-12 16:15:12 +0900

    Feat : 장바구니 리스트이름변경
    장바구니 - 비어있을 경우 출력 기능
    - 장바구니에 있는 메뉴명 출력
    - 총 금액 출력

--- src/com/example/kiosk/Cart.java
+++ src/com/example/kiosk/Cart.java
@@ -4,15 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
-
+    //필드
     private List<MenuItem> cartList;
 
+
+    //생성자
     public Cart(){
         this.cartList = new ArrayList<>();
     }
 
+    //기능
     public void addCart(MenuItem item){
         cartList.add(item);
         System.out.println(item.getName()+"이 장바구니에 추가되었습니다.");
+    }
+
+    public void showCart(){
+        if (cartList.isEmpty()){
+            System.out.println("장바구니가 비어있습니다.");
+            return;
+        }
+        double total = 0;
+        System.out.println("====장바구니====");
+        for (int i =0;i<cartList.size();i++){
+            MenuItem item = cartList.get(i);
+            System.out.println((i+1)+". "+item.getName()+" | ₩ "+item.getPrice()+" | "+item.getInfo());
+            total+=item.getPrice();
+        }
+
+        System.out.println("============================");
+        System.out.println("총 금액 : "+ total);
+        System.out.println("============================");
     }
 }
