a79752a217da941f346ad5289196693b32dd1d4c
escomputer <nujest307@naver.com>
2025-03-13 16:34:31 +0900

    enum으로 할인율 관리
    국가유공자, 군인, 학생, 일반사람에 할인된 가격을 적용하기 위한 실수를 배정하였고 생성자와 기능도 적용했다.

---
+++ src/com/example/kiosk/Discount.java
@@ -0,0 +1,18 @@
+package com.example.kiosk;
+
+public enum Discount {
+    NationalMerit(0.9),
+    Soldier(0.95),
+    Student(0.97),
+    Person(1.0);
+
+    private final double discountPrice;
+
+    Discount(double discountPrice){
+        this.discountPrice = discountPrice;
+    }
+
+    public double discount() {
+        return discountPrice;
+    }
+}
